5e19115009d0d23c6a1609013683dcb8e1f0d4e1
Willy Rodriguez-Jurado <willy@rjurado.com>
2024-07-14 09:39:04 +0200

    Update lambda_function.py

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -13,7 +13,11 @@ def lambda_handler(event, context):
     if event["input"] == "Hello":
 
         return "World"
-
+    
+    if event["input"] == "Hi":
+       
+        return "Hi There"
+        
     else:
 
         raise
